8f341e28b39636cfe70f2829c586ad9594e69f1b
imran farooq <imran_farooq7@hotmail.com>
2022-01-21 06:43:34 +0500

    Header component completed

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,7 +1,34 @@
+import { Autocomplete } from "@react-google-maps/api";
+import { AppBar, Toolbar, Typography, InputBase, Box } from "@mui/material";
+import { Search } from "@mui/icons-material";
+import useStyles from "./styles";
 const Header = () => {
+	const styles = useStyles();
 	return (
 		<div>
-			<h1>Header</h1>
+			<AppBar position="static">
+				<Toolbar className={styles.toolbar}>
+					<Typography variant="h5" className={styles.title}>
+						Traveller
+					</Typography>
+					<Box display="flex">
+						<Typography variant="h6" className={styles.title}>
+							Explore new places
+						</Typography>
+						{/* <Autocomplete> */}
+						<div className={styles.search}>
+							<div className={styles.searchicon}>
+								<Search />
+							</div>
+							<InputBase
+								placeholder="Search..."
+								classes={{ root: styles.inputRoot, input: styles.inputinput }}
+							/>
+						</div>
+						{/* </Autocomplete> */}
+					</Box>
+				</Toolbar>
+			</AppBar>
 		</div>
 	);
 };
